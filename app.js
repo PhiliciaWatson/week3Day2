c27c01f046107d88bdcb1da33c6ea882afc848dc
PhiliciaWatson <philiciawatson7@gmail.com>
2021-04-14 05:29:29 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+var Rosa = 7;
+var Kin = 45;
+
+// Addition
+function Adding(num1,num2){
+
+    return num1 + num2;
+}
+
+console.log(Adding(Rosa,Kin));
+
+
+// Subtraction
+function Sub(num1,num2){
+
+    return num1 - num2;
+}
+
+console.log(Sub(Rosa,Kin));
+
+
+// Multiply
+function Multiply(num1,num2){
+
+    return num1 * num2;
+}
+
+console.log(Multiply(Rosa,Kin));
+
+
+// Divison
+function Divison(num1,num2){
+
+   return num1 / num2;
+
+}
+
+console.log(Divison(Rosa,Kin));
